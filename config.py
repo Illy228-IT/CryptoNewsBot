250599cebcb12b8bf2641bd62ae7e142d61ae096
Illy228-IT <itpython228@gmail.com>
2026-06-01 18:26:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 CHANNEL_ID = os.getenv("CHANNEL_ID")
