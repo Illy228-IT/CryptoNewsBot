d32e91989e7930997b48405db4bbaa208796ceae
Illy228-IT <itpython228@gmail.com>
2026-06-03 20:18:33 +0300

    Update publisher.py

--- tg/publisher.py
+++ tg/publisher.py
@@ -1,19 +1,36 @@
 from telegram import Bot
-from config import BOT_TOKEN, CHANNEL_ID
+
+from config import (
+    BOT_TOKEN,
+    CHANNEL_IDS
+)
 
 bot = Bot(token=BOT_TOKEN)
 
 
 async def publish_post(text):
-    try:
-        await bot.send_message(
-            chat_id=CHANNEL_ID,
-            text=text,
-            parse_mode="HTML",
-            disable_web_page_preview=True
-        )
 
-        print("Telegram message sent")
+    try:
+
+        for channel in CHANNEL_IDS:
+
+            if not channel:
+                continue
+
+            await bot.send_message(
+                chat_id=channel,
+                text=text,
+                parse_mode="HTML",
+                disable_web_page_preview=True
+            )
+
+            print(
+                f"Telegram message sent to "
+                f"{channel}"
+            )
 
     except Exception as e:
-        print(f"Telegram error: {e}")
+
+        print(
+            f"Telegram error: {e}"
+        )
